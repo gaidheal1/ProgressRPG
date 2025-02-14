338a6c67455e7f39a060e35fa31ac9dc91e4199f
Duncan <84990667+gaidheal1@users.noreply.github.com>
2025-02-14 01:40:30 +0000

    Remove dashboard, move stats to profile, add login streak updater signal

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,9 @@
 # user.signals
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, user_logged_in
 from django.dispatch import receiver
+from django.utils.timezone import now
+from datetime import timedelta
 from .models import Profile
 from gameplay.models import ActivityTimer
 from character.models import Character
@@ -28,3 +30,19 @@ def assign_character(sender, instance, created, **kwargs):
     """Create character when profile saved if not already existing"""
     if created:
         assign_character_to_profile(instance)
+
+@receiver(user_logged_in)
+def update_login_streak(sender, request, user, **kwargs):
+    profile = user.profile
+    today = now().date()
+
+    if profile.last_login.date() == today:
+        return  # Already logged in today, no update needed
+
+    if profile.last_login.date() == today - timedelta(days=1):
+        profile.login_streak += 1  # Continue the streak
+    else:
+        profile.login_streak = 1  # Reset streak
+
+    profile.last_login = now()
+    profile.save()
