2f2e390830e21836b1bd9ab758198fdc39858dd4
gaidheal1 <gaidheal01@gmail.com>
2025-02-19 18:51:03 +0000

    New tests; troubleshooting timers

--- character/tests.py
+++ character/tests.py
@@ -1,31 +1,83 @@
 from django.test import TestCase
+from .models import Character, PlayerCharacterLink
+from gameplay.models import QuestCompletion, Quest, QuestTimer
 
 # Create your tests here.
 
 
 
-class TestCharacter(TestCase):
+class TestCharacterCreate(TestCase):
     def test_character_create(self):
         char = Character.objects.create(
-            profile=self.profile,
-            name="Bob"
+            first_name="Bob"
         )
 
         self.assertTrue(isinstance(char, Character))
-        self.assertEqual(char.profile, self.profile)
-        self.assertEqual(char.name, 'Bob')
+        self.assertEqual(char.first_name, 'Bob')
 
-    def test_character_func(self):
-        char = Character.objects.create(
-            profile=self.profile,
-            name="Bob",
-            current_quest = self.quest1,
+class TestCharacterMethods(TestCase):
+    def setUp(self):
+        self.char = Character.objects.create(
+            first_name="Bob",
+            last_name="Bobberson",
+        )
+        self.quest1 = Quest.objects.create(
+            name="Test Quest 1",
+            levelMax=10,
+        )
+        self.timer = QuestTimer.objects.create(
+            character=self.char,
+            quest=self.quest1,
+            duration=10,
+            elapsed_time=5,
         )
 
-        char.complete_quest()
-        completion = QuestCompletion.objects.get(
-                character=char
-        )
+    def test_character_properties(self):
+        self.assertEqual(self.char.full_name, "Bob Bobberson")
+
+    def test_start_quest(self):
+        self.char.start_quest(self.quest1)
+        self.assertEqual(self.timer.quest, self.quest1)
+
+    def test_complete_quest(self):
+        self.char.complete_quest(self.quest1)
+        self.assertEqual(self.char.total_quests, 1)
+        completion = QuestCompletion.objects.filter(
+            character=self.char,
+            quest=self.quest1,
+        ).first()
         self.assertEqual(completion.times_completed, 1)
-        self.assertEqual(char.current_quest, None)
-        self.assertEqual(char.total_quests, 1)
+
+        completions = self.char.get_quest_completions(self.quest1)
+        self.assertEqual(completion, completions[0])
+
+class TestPlayerCharacterLinkCreate(TestCase):
+    def setUp(self):
+        self.char = Character.objects.create(
+            first_name="Bob"
+        )
+
+    def test_link_create(self):
+        #link = PlayerCharacterLink.objects.create(
+        #    profile=self.profile,
+        #    character=self.char,
+        #)
+        pass
+
+class TestPlayerCharacterLinkMethods(TestCase):
+    def setUp(self):
+        self.char = Character.objects.create(
+            first_name="Bob"
+        )
+        #self.link = PlayerCharacterLink.objects.create(
+        #    profile=self.profile,
+        #    character=self.char,
+        #)
+
+    def test_link_func(self):
+        pass
+
+
+
+
+
