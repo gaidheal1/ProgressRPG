0a27260ed22147e8a9bd8fa2685b3b2afcdcd34b
Duncan <84990667+gaidheal1@users.noreply.github.com>
2025-06-21 14:54:03 +0100

    ssl_cert_reqs setting move up a level in CACHES

--- progress_rpg/settings/prod.py
+++ progress_rpg/settings/prod.py
@@ -100,9 +100,7 @@ CACHES = {
         "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            'CONNECTION_POOL_KWARGS': {
-                    'ssl_cert_reqs': ssl.CERT_REQUIRED  if ssl_required else ssl.CERT_NONE,
-                }
+            'ssl_cert_reqs': ssl.CERT_REQUIRED  if ssl_required else ssl.CERT_NONE,
         }
     }
 }
