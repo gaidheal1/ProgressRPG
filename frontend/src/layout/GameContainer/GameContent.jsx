c471d0cbca1bf921fe31e5dc393ef00a340a0a41
gaidheal1 <gaidheal01@gmail.com>
2025-07-03 19:36:35 +0100

    Remove timerprovider context

--- frontend/src/layout/GameContainer/GameContent.jsx
+++ frontend/src/layout/GameContainer/GameContent.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ActivitySection from './ActivitySection/ActivitySection';
 import QuestSection from './QuestSection/QuestSection';
 import styles from './GameContent.module.scss';
-import { TimerProvider } from '../../context/TimerContext';
 
 export default function GameContent() {
 
   return (
     <div className={styles.gameContent}>
-      <TimerProvider>
-        <ActivitySection />
-        <QuestSection />
-      </TimerProvider>
+      <ActivitySection />
+      <QuestSection />
     </div>
   );
 }
