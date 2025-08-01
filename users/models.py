3d83fa807ef83aa830528dd45f8e4db48e861aaf
gaidheal1 <gaidheal01@gmail.com>
2025-08-02 00:44:26 +0100

    add needs_onboarding property

--- users/models.py
+++ users/models.py
@@ -241,6 +241,10 @@ class Profile(Person):
     onboarding_step = models.PositiveIntegerField(choices=ONBOARDING_STEPS, default=0)
 
     @property
+    def needs_onboarding(self):
+        return self.onboarding_step < 4
+
+    @property
     def group_name(self):
         """Returns the WebSocket group name for this profile."""
         return f"profile_{self.id}"
