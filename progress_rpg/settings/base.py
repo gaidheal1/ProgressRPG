be2f06c3b038cffc6b2188a6411b5de57afc040e
gaidheal1 <gaidheal01@gmail.com>
2025-08-11 14:25:59 +0100

    fix: adding frontend folder to django static dirs

--- progress_rpg/settings/base.py
+++ progress_rpg/settings/base.py
@@ -207,7 +207,9 @@ DJANGO_VITE = {
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_DIRS = [Path(BASE_DIR / 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR / "static" / "frontend",
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
