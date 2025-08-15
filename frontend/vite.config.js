552dc0ae67d4e00f4c6d2b0feaf0699f7877afa9
gaidheal1 <gaidheal01@gmail.com>
2025-08-15 12:51:04 +0100

    fix: conditionally set base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/frontend/',
+  base: isProduction ? '/static/frontend/' : '/',
   server: {
     open: true,
     host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
   build: {
     outDir: path.resolve(__dirname, '../static/frontend'),
