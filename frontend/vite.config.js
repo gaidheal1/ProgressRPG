a8f6d43418a9aabddcd3e645a937e7e3c0ddd5d8
gaidheal1 <gaidheal01@gmail.com>
2025-08-18 14:10:21 +0100

    fix: adding different build scripts for local and staging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: isProduction ? '/static/frontend/' : '/',
-  server: {
-    open: true,
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    base: mode === 'staging'
+      ? '/static/frontend/'
+      : '/',
+    server: {
+      open: true,
+      host: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          secure: false,
+        }
       }
-    }
-  },
-  build: {
-    outDir: path.resolve(__dirname, '../static/frontend'),
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: 'src/main.jsx',
     },
-  },
+    build: {
+      outDir: mode === 'staging'
+        ? path.resolve(__dirname, '../static/frontend')
+        : path.resolve(__dirname, './dist'),
+      assetsDir: 'assets',
+      emptyOutDir: true,
+      manifest: true,
+      rollupOptions: {
+        input: 'src/main.jsx',
+      },
+    },
+  }
 })
